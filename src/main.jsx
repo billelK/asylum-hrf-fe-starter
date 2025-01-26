55b124299a1c4c544402f8252e94f8b619ee3e35
billelK <bilalkhansal@gmail.com>
2025-01-26 10:15:49 +0100

    wip

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './App.jsx';
+import { Auth0Provider } from '@auth0/auth0-react';
 import { ProvideAppContext } from './context/AppContext.jsx';
 
 const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
@@ -14,7 +15,15 @@ const AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID;
  * - Set the domain, clientId, and authorizationParams
  */
 createRoot(document.getElementById('root')).render(
+  <Auth0Provider 
+  domain={AUTH_DOMAIN}
+  clientId={AUTH_CLIENT_ID}
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }}
+  >
     <ProvideAppContext>
       <App />
     </ProvideAppContext>
+  </Auth0Provider>
 );
